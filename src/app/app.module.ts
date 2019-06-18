f1d41632bc0726675103f349a43052e086165a63
beaver096 <bal99ram@gmail.com>
2019-06-19 02:07:36 +0530

    Added local project to GIT.

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppHeader } from './header/app-header';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeader,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieDetailComponent,
+    RecepieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
